940e02c5d8dd85093c83708a5b1d2d84f9683520
Marco Robol <marco.robol@unitn.it>
2020-10-12 12:25:09 +0200

    modifiche fatte a lezione

--- REST/products-api/app.js
+++ REST/products-api/app.js
@@ -27,16 +27,35 @@ var products = [{
   description : "Extra large"  
 }];
 
+var max_id = 0;
+
 // Getting a product
 app.get('/api/products/:id', function (req, res) {
   var id = req.params.id;
-  // TODO products[id]
+  var product = products.find( (p) => p.id == id );
+  if(products !== undefined) {
+    res.status(200).send(product);
+  } else {
+    res.status(404);
+  }
+
 });
 
 // Getting the list of product
 
 // Adding a new product
 // TODO products.push(req.body);
+app.post('/api/products', function(req, res) {
+  console.log('POST /api/products ' + req.body.name + ' ' + req.body.description);
+  var p = {
+    id: ++max_id,
+    name: req.body.name,
+    description: req.body.description
+  }
+  products.push(p);
+  res.status(201).location("api/products/" + max_id).send();
+});
+
 
 // Delete a product
 // TODO products.splice(id,1);
